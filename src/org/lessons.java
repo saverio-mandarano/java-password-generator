fbbfa80576cfceaec38c087918999c2d67756fd1
saverio mandarano <saverio.mandarano1@gmail.com>
2026-04-02 18:35:44 +0200

    start  java-password-generator repositorie

---
+++ src/org/lessons.java
@@ -0,0 +1,16 @@
+package org;
+
+public class lessons {
+    static String miaStringa = "stringa1";
+    static String stringa = "stringa2";
+
+    // static int num = 1;
+
+    public static void main(String[] args) {
+        // miaStringa = "arrivederci";
+
+        int num = 1;
+        System.out.println(num);
+    }
+
+}
