857fc615f1ef13b34118493eb18c6c617b928c1f
saverio mandarano <saverio.mandarano1@gmail.com>
2026-04-03 13:20:34 +0200

    Salvate in variabili i dati utente inseriti da tastiera, e generata password

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,34 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main (String[] args){
-        System.out.println("Ciao Saverio");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Inserisci il tuo nome:");
+        String name = input.nextLine();
+        System.out.println("Inserisci il tuo cognome:");
+        String surname = input.nextLine();
+        System.out.println("Inserisci il tuo colore preferito:");
+        String color = input.nextLine();
+
+        System.out.println("Inserisci il tuo giorno di nascita:");
+        int day = input.nextInt();
+        System.out.println("Inserisci il tuo mese di nascita:");
+        int month = input.nextInt();
+        System.out.println("Inserisci il tuo anno di nascita:");
+        int year = input.nextInt();
+
+        int sum = day + month + year;
+        
+        // input.nextLine();
+        // System.out.println("Inserisci stringa di prova:");
+        // String string = input.nextLine();
+        
+        // System.out.println("Password generata: " + name + "-" + surname + "-" + color + "-" + sum + "-" + string);
+        
+        System.out.println("Password generata: " + name + "-" + surname + "-" + color + "-" + sum);
+        
     }
 }
